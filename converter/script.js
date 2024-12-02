74b724d33441d9b2ffe4d474a46e9eb7d018dfbf
teodora-papazova <101417912+teodora-papazova@users.noreply.github.com>
2024-12-02 12:44:26 +0200

    Add files via upload

---
+++ converter/script.js
@@ -0,0 +1,6 @@
+function convert(){
+    var from = document.getElementById('from').value;
+    var to = document.getElementById('to').value;
+    var text = parseFloat(document.getElementById('text').value);
+
+}
